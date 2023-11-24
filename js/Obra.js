de07ba24c24db590af49d41b93d9e30f00ebfef2
tobiasazpia <tobiasazpia@gmail.com>
2023-11-24 18:00:04 -0300

    3raEntrega

--- js/Obra.js
+++ js/Obra.js
@@ -38,53 +38,70 @@ let Obras = [
     }
 ]
 
-//Obtener Indice de la obra en la que estamos
-//To Do: chequear que sea una pagina de obra
-let ObraTitulo = "";
-try{
-    ObraTitulo = document.querySelector("h1").textContent.trim();
-} catch (error) {
-    ObraTitulo = ""
+//Funciones
+
+function SetUpLocalStorage() {
+    //Si no hay local storage, prepararlo; si ya hay, cargarlo en el array de obras
+    let index = 0;
+    if (localStorage.length == 0) {
+        Obras.forEach(element => {
+            localStorage.setItem((index++).toString(), JSON.stringify(element));
+        }
+        )
+    } else {
+        Obras.forEach(element => {
+            Obras[index] = JSON.parse(localStorage.getItem((index++).toString()));
+        })
+    }
 }
 
+//Botones de Obra
+function SetUpBotones() {
+    let btnMG = document.getElementById("btnMeGusta");
+    btnMG.addEventListener("click", clickMG);
+    function clickMG() {
+        contMG.textContent = ++Obras[indexObra].meGusta;
+        localStorage.setItem(indexObra, JSON.stringify(Obras[indexObra]))
+    }
+
+    let btnDescargar = document.getElementById("btnDescargar");
+    btnDescargar.addEventListener("click", clickDescargar);
+    function clickDescargar() {
+        contDescargas.textContent = ++Obras[indexObra].descargas;
+        localStorage.setItem(indexObra, JSON.stringify(Obras[indexObra]))
+    }
+
+    let btnPropina = document.getElementById("btnPropina");
+    btnPropina.addEventListener("click", clickPropina);
+    function clickPropina() {
+        alert("To Do: integracion con APIs de Mercado Pago, Cafecito, etc");
+    }
+
+    let btnSeguir = document.getElementById("btnSeguir");
+    btnSeguir.addEventListener("click", clickSeguir);
+    function clickSeguir() {
+        alert("To Do: Requiere primero implementacion de usuario");
+    }
+
+    let btnReseña = document.getElementById("btnDejarReseña");
+    btnReseña.addEventListener("click", clickDejarReseña);
+    function clickDejarReseña() {
+        alert("To Do: requeriria crear una nueva seccion de html, poniendo un input (?) y despues transformandolo en texto cuando pongan listo");
+    }
+}
+
+//Codigo Principal
+
+SetUpLocalStorage();
+
+//Obtener Indice de la obra en la que estamos
+let ObraTitulo = document.querySelector("h1").textContent.trim();
 let indexObra = Obras.findIndex(element => element.titulo == ObraTitulo);
 
 //Contadores
 let contMG = document.getElementById("contadorMeGusta");
 let contDescargas = document.getElementById("contadorDescargas");
+contMG.textContent = Obras[indexObra].meGusta;
+contDescargas.textContent = Obras[indexObra].descargas;
 
-//Botones de Obra
-let btnMG = document.getElementById("btnMeGusta");
-btnMG.addEventListener("click", clickMG);
-function clickMG()
-{
-    contMG.textContent = ++Obras[indexObra].meGusta;
-}
-
-let btnDescargar = document.getElementById("btnDescargar");
-btnDescargar.addEventListener("click", clickDescargar);
-function clickDescargar()
-{
-    contDescargas.textContent = ++Obras[indexObra].descargas;
-}
-
-let btnPropina = document.getElementById("btnPropina");
-btnPropina.addEventListener("click", clickPropina);
-function clickPropina()
-{
-    alert("To Do: integracion con APIs de Mercado Pago, Cafecito, etc");
-}
-
-let btnSeguir = document.getElementById("btnSeguir");
-btnSeguir.addEventListener("click", clickSeguir);
-function clickSeguir()
-{
-    alert("To Do: Requiere primero implementacion de usuario");
-}
-
-let btnReseña = document.getElementById("btnDejarReseña");
-btnReseña.addEventListener("click", clickDejarReseña);
-function clickDejarReseña()
-{
-    alert("To Do: requeriria crear una nueva seccion de html, poniendo un input (?) y despues transformandolo en texto cuando pongan listo");
-}
+SetUpBotones();
