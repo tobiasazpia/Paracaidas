a8416b90e571ddb04ad9860e91966cbbc1caff3b
Tobias Azpiazu <tobiasazpia@gmail.com>
2023-11-26 02:48:17 -0300

    To be overrided

---
+++ js/3raEntrega.js
@@ -0,0 +1,12 @@
+// Sin prompt ni alert, input y output con DOM 
+// guardando en local Storage
+
+// cada usuario tiene: nombre, mail, contraseña, id unico / si es un array noams el id unico puede ser la posicion en el array
+// cada autor tiene: nombre, info de contacto, about, lista de juegos como autor, lista de juegos como colaborador, y lista de ids de usuarios que lo siguen 
+// cada juego tiene: nombre, tipo, autor, lista de colboradores, contenido, contador de descargas, lista de ids de usuario que likearon, lista de reviews (que a su vez tienen usuario y contenido)
+
+// para la tercer entega, mucha funcionalidad va a quedar en to do, imagino que conun alert, pero otra va a ser iplementeda con local sotrage
+// cada juego tiene: descargar  - alaert to do, like - ls, dejar review - ls, dejar propina - aler to do, seguir autor - ls
+// cada autor tiene dejar propina - alert to do, seguir autor - ls
+
+// entocnes quedan 3 cosas para implementar basicamente, likear y que la compu se acuerde, dejar review y que la compu se acuerda, y seguir a un autor y que la compu se acuerde
