b872b1d3afe11ea85cfaf6736a05ed95bf479466
Tobias Azpiazu <tobiasazpia@gmail.com>
2023-12-15 13:06:42 -0300

    Primeros pasos de grilla en la testing branch

--- js/categoria.js
+++ js/categoria.js
@@ -1,5 +1,5 @@
 let obrasFiltradas;
-let ultimaSeccionB = true;
+let ultimaSeccionR = true;
 
 //Carga la data del JSON / base de datos a un array 
 window.addEventListener("DOMContentLoaded", () => {
@@ -8,32 +8,56 @@ window.addEventListener("DOMContentLoaded", () => {
         return respuesta.json();
     })
     .then((data)=>{
+    //     <div class="grillaL">
+    //     <div class="head">head</div>
+    //     <div class="info">info</div>
+    //     <div class="img">img</div>
+    //   </div>
+    //   <div class="grillaR">
+    //     <div class="head">head</div>
+    //     <div class="info">info</div>
+    //     <div class="img">img</div>
+    //   </div>
         obrasFiltradas = data.filter(esDeEstaCategoria);
         let main = document.querySelector("main");
         main.innerHTML = "";
         obrasFiltradas.forEach(obra => {         
-            const sec = document.createElement("section");
-                if (ultimaSeccionB) {
-                    sec.classList.add("seccionW");
+            const sec = document.createElement("div");
+                if (ultimaSeccionR) {
+
+                    sec.classList.add("grillaL");
                 } else {
-                    sec.classList.add("seccionB");
-                }
-                ultimaSeccionB = !ultimaSeccionB;
+                    sec.classList.add("grillaR");
+                }                
+                ultimaSeccionR = !ultimaSeccionR;
                 main.appendChild(sec);
+
+                const head = document.createElement("div");
+                head.classList.add("head");
+                sec.appendChild(head);
+                const info = document.createElement("div");
+                info.classList.add("info");
+                sec.appendChild(info);
+                const img = document.createElement("div");
+                img.classList.add("img");
+                sec.appendChild(img);
+
                 const titulo = document.createElement("h2");
                 titulo.textContent = obra.titulo;
                 titulo.classList.add("miBotonObra");
-                sec.appendChild(titulo);
+                head.appendChild(titulo);
                 const resumen = document.createElement("p");
                 resumen.textContent = obra.resumen;
-                sec.appendChild(resumen);
+                info.appendChild(resumen);
                 const aut = document.createElement("h4");
                 aut.textContent = obra.autor;
                 aut.classList.add("miBotonAutor");
-                sec.appendChild(aut);
+                info.appendChild(aut);
             
                 titulo.addEventListener('click', pedirObra);
                 aut.addEventListener('click', pedirAutor);
+
+                console.log(main);
         });
     })
     .catch((err)=>{        Swal.fire({
